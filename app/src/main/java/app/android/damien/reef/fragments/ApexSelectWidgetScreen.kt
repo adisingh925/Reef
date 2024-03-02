1051eeec3d3f54b2da0a15808afc5f26209ec97f
adisingh925 <adisingh925@gmail.com>
2024-03-02 12:42:31 +0530

    custom widgets done

--- app/src/main/java/app/android/damien/reef/fragments/ApexSelectWidgetScreen.kt
+++ app/src/main/java/app/android/damien/reef/fragments/ApexSelectWidgetScreen.kt
@@ -7,6 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import app.android.damien.reef.R
 import app.android.damien.reef.databinding.FragmentApexSelectWidgetScreenBinding
+import app.android.damien.reef.storage.SharedPreferences
+import app.android.damien.reef.utils.Constants
+import app.android.damien.reef.utils.Toast
 
 
 class ApexSelectWidgetScreen : Fragment() {
@@ -19,7 +22,94 @@ class ApexSelectWidgetScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
+
+        binding.apexFlaskBackgroundWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_FLASK_BACKGROUND_WIDGET, 0).toString() + "/5 widgets added"
+        binding.apexCircleWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_CIRCLE_WIDGET, 0).toString() + "/5 widgets added"
+        binding.apex2RectangleWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_TWO_RECTANGLE_WIDGET, 0).toString() + "/5 widgets added"
+        binding.apexSingleValueType1.itemSubheading.text = SharedPreferences.read(Constants.APEX_SINGLE_VALUE_TYPE_1_WIDGET, 0).toString() + "/5 widgets added"
+        binding.apexSingleValueType2.itemSubheading.text = SharedPreferences.read(Constants.APEX_SINGLE_VALUE_TYPE_2_WIDGET, 0).toString() + "/5 widgets added"
+        binding.apexPowerValuesWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_POWER_VALUE_WIDGET, 0).toString() + "/5 widgets added"
+        binding.apexWaterQualityWidget.itemSubheading.text = SharedPreferences.read(Constants.APEX_WATER_QUALITY_WIDGET, 0).toString() + "/5 widgets added"
+
+        binding.apexFlaskBackgroundWidgets.flaskConstraintLayout.setOnClickListener {
+            val widgetCount = SharedPreferences.read(Constants.APEX_FLASK_BACKGROUND_WIDGET, 0)
+            if (widgetCount in 0..4) {
+                SharedPreferences.write(Constants.APEX_FLASK_BACKGROUND_WIDGET, widgetCount + 1)
+            } else {
+                Toast.showSnackbar(binding.root, "You can only add 5 widgets")
+            }
+
+            binding.apexFlaskBackgroundWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_FLASK_BACKGROUND_WIDGET, 0).toString() + "/5 widgets added"
+        }
+
+        binding.apexCircleWidgets.flaskConstraintLayout.setOnClickListener {
+            val widgetCount = SharedPreferences.read(Constants.APEX_CIRCLE_WIDGET, 0)
+            if (widgetCount in 0..4) {
+                SharedPreferences.write(Constants.APEX_CIRCLE_WIDGET, widgetCount + 1)
+            } else {
+                Toast.showSnackbar(binding.root, "You can only add 5 widgets")
+            }
+
+            binding.apexCircleWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_CIRCLE_WIDGET, 0).toString() + "/5 widgets added"
+        }
+
+        binding.apex2RectangleWidgets.flaskConstraintLayout.setOnClickListener {
+            val widgetCount = SharedPreferences.read(Constants.APEX_TWO_RECTANGLE_WIDGET, 0)
+            if (widgetCount in 0..4) {
+                SharedPreferences.write(Constants.APEX_TWO_RECTANGLE_WIDGET, widgetCount + 1)
+            } else {
+                Toast.showSnackbar(binding.root, "You can only add 5 widgets")
+            }
+
+            binding.apex2RectangleWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_TWO_RECTANGLE_WIDGET, 0).toString() + "/5 widgets added"
+        }
+
+        binding.apexSingleValueType1.flaskConstraintLayout.setOnClickListener {
+            val widgetCount = SharedPreferences.read(Constants.APEX_SINGLE_VALUE_TYPE_1_WIDGET, 0)
+            if (widgetCount in 0..4) {
+                SharedPreferences.write(Constants.APEX_SINGLE_VALUE_TYPE_1_WIDGET, widgetCount + 1)
+            } else {
+                Toast.showSnackbar(binding.root, "You can only add 5 widgets")
+            }
+
+            binding.apexSingleValueType1.itemSubheading.text = SharedPreferences.read(Constants.APEX_SINGLE_VALUE_TYPE_1_WIDGET, 0).toString() + "/5 widgets added"
+        }
+
+        binding.apexSingleValueType2.flaskConstraintLayout.setOnClickListener {
+            val widgetCount = SharedPreferences.read(Constants.APEX_SINGLE_VALUE_TYPE_2_WIDGET, 0)
+            if (widgetCount in 0..4) {
+                SharedPreferences.write(Constants.APEX_SINGLE_VALUE_TYPE_2_WIDGET, widgetCount + 1)
+            } else {
+                Toast.showSnackbar(binding.root, "You can only add 5 widgets")
+            }
+
+            binding.apexSingleValueType2.itemSubheading.text = SharedPreferences.read(Constants.APEX_SINGLE_VALUE_TYPE_2_WIDGET, 0).toString() + "/5 widgets added"
+        }
+
+        binding.apexPowerValuesWidgets.flaskConstraintLayout.setOnClickListener {
+            val widgetCount = SharedPreferences.read(Constants.APEX_POWER_VALUE_WIDGET, 0)
+            if (widgetCount in 0..4) {
+                SharedPreferences.write(Constants.APEX_POWER_VALUE_WIDGET, widgetCount + 1)
+            } else {
+                Toast.showSnackbar(binding.root, "You can only add 5 widgets")
+            }
+
+            binding.apexPowerValuesWidgets.itemSubheading.text = SharedPreferences.read(Constants.APEX_POWER_VALUE_WIDGET, 0).toString() + "/5 widgets added"
+        }
+
+        binding.apexWaterQualityWidget.flaskConstraintLayout.setOnClickListener {
+            val widgetCount = SharedPreferences.read(Constants.APEX_WATER_QUALITY_WIDGET, 0)
+            if (widgetCount in 0..4) {
+                SharedPreferences.write(Constants.APEX_WATER_QUALITY_WIDGET, widgetCount + 1)
+            } else {
+                Toast.showSnackbar(binding.root, "You can only add 5 widgets")
+            }
+
+            binding.apexWaterQualityWidget.itemSubheading.text = SharedPreferences.read(Constants.APEX_WATER_QUALITY_WIDGET, 0).toString() + "/5 widgets added"
+        }
+
+
+
         return binding.root
     }
 }
--- app/src/main/java/app/android/damien/reef/utils/Constants.kt
+++ app/src/main/java/app/android/damien/reef/utils/Constants.kt
@@ -6,4 +6,13 @@ object Constants {
     const val CUSTOM = 3
 
     const val CUSTOM_WIDGET_TABLE = "custom_widget_table"
+
+
+    const val APEX_FLASK_BACKGROUND_WIDGET = "AFBW"
+    const val APEX_POWER_VALUE_WIDGET = "APVW"
+    const val APEX_CIRCLE_WIDGET = "ACW"
+    const val APEX_TWO_RECTANGLE_WIDGET = "ATRW"
+    const val APEX_SINGLE_VALUE_TYPE_1_WIDGET = "ASVT1W"
+    const val APEX_SINGLE_VALUE_TYPE_2_WIDGET = "ASVT2W"
+    const val APEX_WATER_QUALITY_WIDGET = "AWQW"
 }
