1d2895a6daa03dbc7f02c51a2c1e2b6e0b752da3
Aditya Singh <adisingh925@gmail.com>
2024-05-04 12:10:00 +0530

    all apis added

--- app/src/main/java/app/android/damien/reef/apis/ApiService.kt
+++ app/src/main/java/app/android/damien/reef/apis/ApiService.kt
@@ -20,17 +20,17 @@ interface ApiService {
      * Apex APIs
      */
 
-    @POST("https://www.impromptureef.co.uk/api/cookies/fusion")
+    @POST("${CUSTOM_BASE_URL}cookies/fusion")
     fun apexUserLogin(
         @Body request: LoginRequest,
     ): Call<LoginResponse>
 
-    @POST("https://www.impromptureef.co.uk/api/cookies/fusion/refresh")
+    @POST("${CUSTOM_BASE_URL}cookies/fusion/refresh")
     fun apexRefreshCookie(
         @Body request: LoginRequest
     ): Call<LoginResponse>
 
-    @GET("https://apexfusion.com/api/apex?page=1&per_page=20&total_pages=1&total_entries=1")
+    @GET("${APEX_BASE_URL}apex?page=1&per_page=20&total_pages=1&total_entries=1")
     fun getApexData(
         @Header("Cookie") cookie: String,
     ): Call<ApexData>
@@ -39,36 +39,42 @@ interface ApiService {
      * Focustronic APIs
      */
 
-    @POST("https://www.impromptureef.co.uk/api/cookies/focustronic")
+    @POST("${CUSTOM_BASE_URL}cookies/focustronic")
     fun focustronicUserLogin(
         @Body request: LoginRequest,
     ): Call<LoginResponse>
 
-    @POST("https://www.impromptureef.co.uk/api/cookies/focustronic/refresh")
+    @POST("${CUSTOM_BASE_URL}cookies/focustronic/refresh")
     fun focustronicRefreshCookie(
         @Body request: LoginRequest
     ): Call<LoginResponse>
 
-    @GET("https://alkatronic.focustronic.com/api/v2/aquarium-tanks")
+    @GET("${ALKATRONIC_BASE_URL}/api/v2/aquarium-tanks")
     fun getAquariumTanks(
         @Header("x-session-token") token: String,
     ): Call<AquariumTanks>
 
-    @GET("https://alkatronic.focustronic.com/api/v2/aquarium-tanks/{id}")
+    @GET("${ALKATRONIC_BASE_URL}/api/v2/aquarium-tanks/{id}")
     fun getAquariumDevices(
         @Header("x-session-token") token: String,
         @Path("id") tankId: Int
     ): Call<AquariumDevices>
 
-    @GET("https://alkatronic.focustronic.com/api/v2/devices/mastertronic/{id}/parameter-information")
+    @GET("${ALKATRONIC_BASE_URL}/api/v2/devices/mastertronic/{id}/parameter-information")
     fun getMastertronicData(
         @Header("x-session-token") token: String,
         @Path("id") tankId: Int
     ): Call<MastertronicData>
 
-    @GET("https://alkatronic.focustronic.com/api/v2/devices/alkatronic/{id}/data/test-records?day=1")
+    @GET("${ALKATRONIC_BASE_URL}/api/v2/devices/alkatronic/{id}/data/test-records?day=1")
     fun getAlkatronicData(
         @Header("x-session-token") token: String,
         @Path("id") tankId: Int
     ): Call<AlkatronicData>
+
+    companion object{
+        const val CUSTOM_BASE_URL = "https://www.impromptureef.co.uk/api/"
+        const val ALKATRONIC_BASE_URL = "https://alkatronic.focustronic.com"
+        const val APEX_BASE_URL = "https://apexfusion.com/api/"
+    }
 }
