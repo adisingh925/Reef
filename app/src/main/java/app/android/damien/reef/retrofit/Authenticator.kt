57fe7983f0d7796435e49b5e750bb8a14aa9a1e9
Aditya Singh <adisingh925@gmail.com>
2024-05-07 23:06:43 +0530

    auto cookie refresh

---
+++ app/src/main/java/app/android/damien/reef/retrofit/Authenticator.kt
@@ -0,0 +1,72 @@
+package app.android.damien.reef.retrofit
+
+import android.util.Log
+import app.android.damien.reef.model.login.LoginRequest
+import app.android.damien.reef.storage.SharedPreferences
+import app.android.damien.reef.utils.Constants
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+
+class Authenticator : Authenticator {
+    override fun authenticate(route: Route?, response: Response): Request? {
+        val requestUrl = response.request().url()
+        val baseUrl = requestUrl.host()
+
+        if (baseUrl.equals("apexfusion.com")) {
+            Log.d("Authenticator", "apexfusion.com")
+
+            val refreshRequest = ApiClient.customApiService.apexRefreshCookie(
+                LoginRequest(
+                    SharedPreferences.read(
+                        "${Constants.APEX}username",
+                        ""
+                    ).toString(),
+                    SharedPreferences.read("${Constants.APEX}password", "").toString()
+                )
+            )
+
+            val result = refreshRequest.execute()
+
+            if (result.isSuccessful) {
+                val newCookie = result.body()?.sid
+                SharedPreferences.write("${Constants.APEX}cookie", newCookie.toString())
+
+                return response
+                    .request()
+                    .newBuilder()
+                    .header("Cookie", "connect.sid=${newCookie}")
+                    .build()
+            }
+        } else if (baseUrl.equals("alkatronic.focustronic.com")) {
+            Log.d("Authenticator", "alkatronic.focustronic.com")
+            val refreshRequest = ApiClient.customApiService.focustronicRefreshCookie(
+                LoginRequest(
+                    SharedPreferences.read(
+                        "${Constants.FOCUSTRONIC}username",
+                        ""
+                    ).toString(),
+                    SharedPreferences.read("${Constants.FOCUSTRONIC}password", "").toString()
+                )
+            )
+
+            val result = refreshRequest.execute()
+
+            if (result.isSuccessful) {
+                val newCookie = result.body()?.sid
+                SharedPreferences.write("${Constants.FOCUSTRONIC}cookie", newCookie.toString())
+
+                return response
+                    .request()
+                    .newBuilder()
+                    .header("x-session-token", newCookie.toString())
+                    .build()
+            }
+        } else
+            Log.d("Authenticator", "Unknown URL: $baseUrl")
+
+
+        return null
+    }
+}
--- app/src/main/java/app/android/damien/reef/retrofit/RetrofitAndApiService.kt
+++ app/src/main/java/app/android/damien/reef/retrofit/RetrofitAndApiService.kt
@@ -1,14 +1,24 @@
 package app.android.damien.reef.retrofit
 
 import app.android.damien.reef.apis.ApiService
+import okhttp3.ConnectionSpec
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
+
+    private val client: OkHttpClient = OkHttpClient().newBuilder()
+        .authenticator(Authenticator())
+        .retryOnConnectionFailure(true)
+        .connectionSpecs(listOf(ConnectionSpec.MODERN_TLS))
+        .build()
 
     val apexRetrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://apexfusion.com/api/")
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -22,6 +32,7 @@ object RetrofitClient {
 
     val alkatronicRetrofit: Retrofit by lazy {
         Retrofit.Builder()
+            .client(client)
             .baseUrl("https://alkatronic.focustronic.com/api/v2/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
