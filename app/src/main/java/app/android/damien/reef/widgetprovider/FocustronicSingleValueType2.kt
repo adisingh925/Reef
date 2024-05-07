8fb48374695240ea0548c47cc79ec76528c69648
Aditya Singh <adisingh925@gmail.com>
2024-05-08 02:59:48 +0530

    widget tap to refresh

--- app/src/main/java/app/android/damien/reef/widgetprovider/FocustronicSingleValueType2.kt
+++ app/src/main/java/app/android/damien/reef/widgetprovider/FocustronicSingleValueType2.kt
@@ -1,6 +1,16 @@
 package app.android.damien.reef.widgetprovider
 
+import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.Context
 
 class FocustronicSingleValueType2 : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
 }
